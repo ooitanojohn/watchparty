3125ac72daf361fe2e1deb388918713f7ac20088
ooitanojohn <qmkh733310@gmail.com>
2022-12-14 11:34:05 +0900

    [chore] firebaseSDK install

---
+++ utils/firebase/init.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmRdNM-c12IBU8Xeo66N9j_HXtMC6uTCE",
+  authDomain: "watch-party-2b5cd.firebaseapp.com",
+  projectId: "watch-party-2b5cd",
+  storageBucket: "watch-party-2b5cd.appspot.com",
+  messagingSenderId: "519775775696",
+  appId: "1:519775775696:web:ef890b8dcb3e0dd394e7d5",
+  measurementId: "G-KZDPJPFYKF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
